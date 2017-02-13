eafe4124fdc31d695f25931f0a56fb17be4a8dd9
Vincent Velarde <vinvelarde@tamu.edu>
2017-02-13 01:46:35 -0600

    Update fbsd.cpp

--- fbsd.cpp
+++ fbsd.cpp
@@ -17,8 +17,12 @@
 //-------------------------------------------------------------
 
 #include <iostream>
+#include <fstream>
 #include <memory>
 #include <string>
+#include <chrono>
+#include <ctime>
+#include <vector>
 #include <grpc++/grpc++.h>
 
 #include "fb.grpc.pb.h"
@@ -28,33 +32,112 @@ using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::ServerWriter;
 using grpc::Status;
+using HW2::UserBase;
 using HW2::User;
 using HW2::Action;
 using HW2::SendMsg;
 using HW2::Tweeter;
+using std::chrono::system_clock;
 
 //this class handles the RPCs
 class masterServer final : public Tweeter::Service {
+	UserBase userList;
+	
+	//Welcome RPC
+	//checks client username
+	//	adds client to user list if they aren't already there
+	//	creates client file if one doesn't already exist
+	Status Welcome(ServerContext* context, const HW2::User* user,
+					SendMsg* welcome) {
+		//first, check if user already exists
+		User u;
+		welcome->set_sender("SERVER: ");
+		for(int i=0; i<userList.user_size(); i++) {
+			u = userList.user(i);
+			if(u.username() == user->username()) {	//if user exists, welcome back
+				welcome->set_message("Welcome Back!");
+				return Status::OK;
+			}
+		}
+		
+		//if user doesn't exist add them to userList
+		User* newUser = userList.add_user();
+		newUser->set_username(user->username());
+		newUser->add_followers(user->username());
+		std::filebuf fb;
+		fb.open("UserList.txt", std::ios::out);
+		std::ostream os(&fb);
+		userList.SerializeToOstream(&os);
+		fb.close();
+		
+		welcome->set_message("Welcome to the Server!");
+		
+		return Status::OK;
+	}
+	
 	//LIST command
-	//UNDER CONSTRUCTION
 	//Sends the client a list of all users indicating which ones
 	//	the client is currently following
 	Status List(ServerContext* context, const HW2::User* user,
 				ServerWriter<User>* writer) override {
 		User u;
-		u.set_username("UNDER CONSTRUCTION");
-		writer->Write(u);
+		//iterate through userList and send each user
+		for(int i=0; i<userList.user_size(); i++) {
+			u = userList.user(i);
+			//iterate through followers and check if the current user is following User u
+			for(int j=0; j<u.followers_size(); j++) {
+				if(u.followers(j) == user->username()) {
+					u.set_following(true);
+				}
+			}
+			writer->Write(u);
+		}
 		
 		return Status::OK;
 	}
 	
 	//JOINT and LEAVE commands
-	//UNDER CONSTRUCTION
 	//Follows or unfollows a user for the client
 	Status Following(ServerContext* context, const Action* action,
 							SendMsg* msg) override {
+		User* u;
+		//iterate through userList and find the username to follow/unfollow
+		for(int i=0; i<userList.user_size(); i++) {
+			u = userList.mutable_user(i);
+			if(u->username() == action->username()) {
+				if(action->act()) {	//if this is a JOIN command
+					u->add_followers(action->currentuser());
+					break;
+				}
+				else {	//if this is a LEAVE command
+					User temp;
+					temp.set_username(u->username());
+					
+					//copy over all the followers other than you to temp
+					for(int j=0; j<u->followers_size(); j++) {
+						if(u->followers(j) != action->currentuser())
+							temp.add_followers(u->followers(j));
+					}
+					
+					//clear the followers list and copy back the new list from temp
+					u->clear_followers();
+					for(int j=0; j<temp.followers_size(); j++) {
+						u->add_followers(temp.followers(j));
+					}
+					break;
+				}
+			}
+		}
+		
+		std::filebuf fb;
+		fb.open("UserList.txt", std::ios::out);
+		std::ostream os(&fb);
+		userList.SerializeToOstream(&os);
+		fb.close();
+		
 		msg->set_sender("SERVER: ");
-		msg->set_message("UNDER CONSTRUCTION");
+		msg->set_message(action->username());
+		
 		return Status::OK;
 	}
 	
@@ -62,10 +145,54 @@ class masterServer final : public Tweeter::Service {
 	//UNDER CONSTRUCTION
 	Status Msg(ServerContext* context, const SendMsg* msg,
 				SendMsg* confirm) override {
-		confirm->set_sender("SERVER: ");
-		confirm->set_message("UNDER CONSTRUCTION");
+		system_clock::time_point today = system_clock::now();
+		time_t tt;
+		tt = system_clock::to_time_t(today);
+		
+		User* u;
+		std::vector<std::string> followers;
+		//iterate through userList and find sender
+		for(int i=0; i<userList.user_size(); i++) {
+			u = userList.mutable_user(i);
+			if(u->username() == msg->sender()) {
+				//make list of followers
+				for(int j=0; j<u->followers_size(); j++) {
+					followers.push_back(u->username());
+				}
+				break;
+			}
+		}
+		//iterate through and find followers
+		for(int i=0; i<userList.user_size(); i++) {
+			u = userList.mutable_user(i);
+			for(int j=0; j<followers.size(); j++) {
+				if(u->username() == followers[j]) {
+					SendMsg* message = u->add_userlog();
+					message->set_sender(msg->sender());
+					message->set_timestamp(ctime(&tt));
+					message->set_message(msg->message());
+				}
+			}
+		}
+		
+		std::filebuf fb;
+		fb.open("UserList.txt", std::ios::out);
+		std::ostream os(&fb);
+		userList.SerializeToOstream(&os);
+		fb.close();
+		
 		return Status::OK;
 	}
+	
+	public:
+		//read current user list from file at server start
+		void getUserList() {
+			std::filebuf fb;
+			fb.open("UserList.txt", std::ios::in);
+			std::istream os(&fb);
+			userList.ParseFromIstream(&os);
+			fb.close();
+		}
 };
 
 //setup and run the server
@@ -81,6 +208,7 @@ void runServer(char* p) {
 	builder.RegisterService(&service);
 	std::unique_ptr<Server> server(builder.BuildAndStart());
 	std::cout << "Server listening on " << server_address << std::endl;
+	service.getUserList();
 	
 	server->Wait();
 }
@@ -91,6 +219,35 @@ int main(int argc, char** argv) {
 		fprintf(stderr, "USAGE: %s <port>\n", argv[0]);
 		exit(1);
 	}
+	
+	//------------------------------------------------------
+	//THIS CODE WRITES A PROTO BUFFER MESSAGE TO A FILE
+	//------------------------------------------------------
+	std::filebuf fb;
+	fb.open("UserList.txt", std::ios::out);
+	std::ostream os(&fb);
+	UserBase userList;
+	User* user1 = userList.add_user();
+	user1->set_username("Vince");
+	user1->add_followers("Vince");
+	user1->add_followers("Vincent");
+	user1 = userList.add_user();
+	user1->set_username("Ruth");
+	user1->add_followers("Ruth");
+	user1 = userList.add_user();
+	user1->set_username("Sarah");
+	user1->add_followers("Sarah");
+	user1->add_followers("Vincent");
+	user1 = userList.add_user();
+	user1->set_username("Vincent");
+	user1->add_followers("Vincent");
+	user1 = userList.add_user();
+	user1->set_username("Joseph");
+	user1->add_followers("Joseph");
+	user1->add_followers("Ruth");
+	user1->add_followers("Sarah");
+	userList.SerializeToOstream(&os);
+	fb.close();
 	
 	runServer(argv[1]);
 	
