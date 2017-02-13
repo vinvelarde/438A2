491c5c03985ddb843334611b661c489634aad1c3
Vincent Velarde <vinvelarde@tamu.edu>
2017-02-13 01:46:56 -0600

    Update fbc.cpp

--- fbc.cpp
+++ fbc.cpp
@@ -38,26 +38,21 @@ class TweeterClient {
 		TweeterClient(std::shared_ptr<Channel> channel)
 			: stub_(Tweeter::NewStub(channel)) {}
 	
-	//used for LIST command
-	//UNDER CONSTRUCTION
-	//Sends the client a list of all users indicating which ones
-	//	the client is currently following
-	std::string List(const std::string& u) {
+	//called when joining server
+	//sends username of client to server and recieves welcome message
+	std::string Welcome(const std::string& user) {
 		//set up RPC inputs
-		User user, reply;
-		user.set_username(u);
+		User u;
+		SendMsg msg;
+		u.set_username(user);
 		ClientContext context;
 		
-		//loop through returning stream
-		std::unique_ptr<ClientReader<User> > reader(stub_->List(&context, user));
-		while(reader->Read(&reply)) {
-			//print list of users from here
-		}
-		Status status = reader->Finish();
+		//Welcome RPC
+		Status status = stub_->Welcome(&context, u, &msg);
 		
-		//do something with return value
+		//do something with the return value
 		if(status.ok()) {
-			return reply.username();
+			return msg.message();
 		}
 		else {
 			std::cout << status.error_code() << ": " << status.error_message()
@@ -66,8 +61,30 @@ class TweeterClient {
 		}
 	}
 	
+	//used for LIST command
+	//Sends the client a list of all users indicating which ones
+	//	the client is currently following
+	void List(const std::string& u) {
+		//set up RPC inputs
+		User user, reply;
+		user.set_username(u);
+		ClientContext context;
+		
+		//loop through returning stream
+		std::unique_ptr<ClientReader<User> > reader(stub_->List(&context, user));
+		while(reader->Read(&reply)) {
+			//print user list and indicate which users have been followed by client
+			if(reply.username() == u)
+				std::cout << reply.username() << " - You" << std::endl;
+			else if(reply.following())
+				std::cout << reply.username() << " - Followed" << std::endl;
+			else
+				std::cout << reply.username() << std::endl;
+		}
+		Status status = reader->Finish();
+	}
+	
 	//used for JOIN and LEAVE commands
-	//UNDER CONSTRUCTION
 	//bool act represents whether to follow or unfollow
 	//	true = follow, false = unfollow
 	std::string Following(const std::string& currentUser, std::string& user, bool act) {
@@ -130,17 +147,18 @@ void cmdLoop(std::string user, TweeterClient* tweeter) {
 		getline(std::cin, input);
 		
 		//handle commands from user input
-		if(input == "LIST") {	//needs to print out list of users (mark followed users with '*' maybe?)
-			reply = tweeter->List(user);
-			std::cout << "List RPC response: " << reply << std::endl;
+		if(input == "LIST") {
+			tweeter->List(user);
 		}
-		else if(input == "JOIN") {	//needs to take additional input (username to join)
-			reply = tweeter->Following(user, user, false);
-			std::cout << "Following RPC response: " << reply << std::endl;
+		else if(input.substr(0,4) == "JOIN") {
+			std::string follow = input.substr(5, input.length()-5);
+			reply = tweeter->Following(user, follow, true);
+			std::cout << "Now Following: " << reply << std::endl;
 		}
-		else if(input == "LEAVE") {	//needs to take additional input (username to leave)
-			reply = tweeter->Following(user, user, false);
-			std::cout << "Following RPC response: " << reply << std::endl;
+		else if(input.substr(0,5) == "LEAVE") {
+			std::string unfollow = input.substr(6, input.length()-6);
+			reply = tweeter->Following(user, unfollow, false);
+			std::cout << "Unfollowed: " << reply << std::endl;
 		}
 		else if(input == "CHAT") break;	//break cmdLoop and move to chatLoop
 		else std::cout << "Invalid Input" << std::endl;
@@ -158,7 +176,7 @@ void chatLoop(std::string user, TweeterClient* tweeter) {
 		getline(std::cin, input);
 		
 		reply = tweeter->Msg(user);	//needs to take additional input (chat message)
-		std::cout << "Msg RPC response: " << reply << std::endl;
+		//std::cout << "Msg RPC response: " << reply << std::endl;
 	}
 }
 
@@ -178,12 +196,20 @@ int main(int argc, char** argv) {
 	TweeterClient tweeter(grpc::CreateChannel(
       server_address, grpc::InsecureChannelCredentials()));
 	std::string user(argv[3]);
-
+	
+	std::string serverMsg = tweeter.Welcome(user);
+	std::cout << serverMsg << std::endl;
+	
 	cmdLoop(user, &tweeter);
 	chatLoop(user, &tweeter);
 	
 	return 0;
 }
+
+
+
+
+
 
 
 
